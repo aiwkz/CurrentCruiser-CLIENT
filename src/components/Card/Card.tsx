753ea7b6050025436e8cb64624d55ee86858f54a
aiwkz <federicoj.pp@gmail.com>
2026-02-06 13:49:11 +0100

    ⬆️ updates dependecies adnd fixes errors

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,20 +3,20 @@ import { Car } from '@/types';
 import { useCarsStore } from '@/stores/carsStore';
 
 interface CardProps {
-  _id: string;
+  id: string;
   img: string;
   name: string;
   description: string;
 }
 
-const Card = ({ _id, img, name, description }: CardProps) => {
+const Card = ({ id, img, name, description }: CardProps) => {
   const cars = useCarsStore(state => state.cars);
   const setCurrentCar = useCarsStore(state => state.setCurrentCar);
   const navigate = useNavigate();
   const { VITE_BACKEND_URL } = import.meta.env;
 
   const handleCardClick = () => {
-    const selectedCar = cars.find((car: Car) => car._id === _id);
+    const selectedCar = cars.find((car: Car) => car._id === id);
     if (selectedCar) {
       setCurrentCar(selectedCar);
       navigate('/car-details');
@@ -28,7 +28,7 @@ const Card = ({ _id, img, name, description }: CardProps) => {
       <img
         alt='Car image'
         className='Card-img'
-        src={`${VITE_BACKEND_URL}/${img}`}
+        src={`${VITE_BACKEND_URL}/assets/images/${img}`}
       />
       <div className='Card-name-description-container'>
         <div className='Card-name'>{name}</div>
